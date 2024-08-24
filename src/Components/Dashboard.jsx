c2a5490b3e31e7490682bcc4874212b8c1be733a
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2024-08-24 19:29:18 +0530

    RESET PROJECT

--- src/Components/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Dashboard() {
-  return (
-    <div  className='flex justify-center items-center'><p>Welcome to study</p></div>
-  )
-}
-
-export default Dashboard
